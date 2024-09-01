e787a7b52d804ea66c6853040d04fb6183cd160d
Amir <apple@Apples-MacBook-Air.local>
2024-09-01 22:07:35 +0330

    upvote bug

--- src/components/upvote/Upvote.jsx
+++ src/components/upvote/Upvote.jsx
@@ -4,12 +4,10 @@ import axios from "axios";
 
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
-
 import { ChevronUp } from "lucide-react";
 
 const Upvote = ({ product, user, isReview = false }) => {
   const queryClient = useQueryClient();
-
   const router = useRouter();
 
   const mutation = useMutation({
@@ -30,15 +28,18 @@ const Upvote = ({ product, user, isReview = false }) => {
     }
   };
 
+  // Check if user is null before accessing user._id
+  const userHasVoted = user ? product.upvotes.some((u) => u._id === user._id) : false;
+
   return (
     <button
       onClick={handleUpvote}
       className={`btn !flex !flex-row ${
         isReview
-          ? product.upvotes.includes(user._id)
+          ? userHasVoted
             ? "btn-primary"
             : "btn-neutral"
-          : product.upvotes.some((u) => u._id === user._id)
+          : userHasVoted
             ? "btn-primary"
             : "btn-neutral"
       } ${isReview ? "btn-square" : ""}`}
@@ -47,9 +48,7 @@ const Upvote = ({ product, user, isReview = false }) => {
         <ChevronUp size={22} className="w-full" />
       ) : (
         <p>
-          {product.upvotes.some((u) => u._id === user._id)
-            ? "گرفتن رای"
-            : "رای  دادن"}
+          {userHasVoted ? "گرفتن رای" : "رای دادن"}
         </p>
       )}
       <div className="-mt-3 text-xs">{product.upvotes.length}</div>
